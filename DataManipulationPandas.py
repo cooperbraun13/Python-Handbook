1428b7e873938258552651dff94f4874cba00b14
cooperbraun13 <cooperjbraun13@gmail.com>
2025-03-12 01:13:44 -0700

    finished quick section lol

---
+++ DataManipulationPandas.py
@@ -0,0 +1,9 @@
+""" 
+Installing and Using Pandas
+"""
+# Once pandas is installed, you can import it and check the version
+import pandas
+print(pandas.__version__)
+
+# Import pandas under the alias pd
+import pandas as pd
